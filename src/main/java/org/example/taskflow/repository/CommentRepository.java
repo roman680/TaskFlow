82fb29d0b01f33aa9a89b2c90c118a3f30bee484
Roman Denysiuk <r.v.denysiuk@gmail.com>
2025-03-27 23:04:11 +0100

    created Repositories

---
+++ src/main/java/org/example/taskflow/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package org.example.taskflow.repository;
+
+import org.example.taskflow.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByTaskId(Long taskId);
+}
---
+++ src/main/java/org/example/taskflow/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package org.example.taskflow.repository;
+
+import org.example.taskflow.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
